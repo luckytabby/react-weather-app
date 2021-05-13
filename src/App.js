0922e5776e45242eb9d75787f824b6aefa28aad6
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-13 12:09:20 -0400

    Changed background color and added title
    Changed background color to light pink and added a title and heading that says "Pug Weather"; still need to draw pugs and work on visual content.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pug Weather by Maggie McDonald
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
