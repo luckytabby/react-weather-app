97991a85903cdf000b70e46aff9b11eafa7a900b
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-13 14:49:51 -0400

    Added links to signature

--- src/App.js
+++ src/App.js
@@ -8,10 +8,14 @@ function App() {
           Pug Weather by Maggie McDonald
         </p>
       </header>
-      <p>This project was coded by Maggie McDonald and is 
-        <a href="git@github.com:maggiegmcd/react-weather-app.git">
-          open-sourced.
-        </a>
+      <p>This project was coded by
+          <a href="linkedin.com/maggiegmcd">
+             Maggie McDonald
+          </a> 
+          and is 
+          <a href="git@github.com:maggiegmcd/react-weather-app.git">
+             open-sourced.
+          </a>
       </p>
     </div>
   );
