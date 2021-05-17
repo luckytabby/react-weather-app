62b9ad91b7f4755ac08a69d79e15c0731f026e13
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-17 17:56:29 -0400

    Changed CSS background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Weather from './Weather'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 function App() {
   return (
@@ -7,11 +8,13 @@ function App() {
         <h1>Pug Weather</h1>
         <Weather />
         <p className="signature">
+            <FontAwesomeIcon icon="fas fa-paw" />
             This project is <a href="https://github.com/maggiegmcd/react-weather-app">open-sourced</a> and 
             coded by <a href="https://www.linkedin.com/in/maggiegmcd/">Maggie McDonald</a>.
         </p>
     </div>
   );
+
 }
 
 export default App;
