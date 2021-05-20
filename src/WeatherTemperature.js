13b48740aaa8ff28bfc779ba674595dfd7431f8e
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-20 16:21:08 -0400

    Added °C/F conversion feature
    Added a component to convert temperature to Celsius and/or back to Fahrenheit.

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,10 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
+import "./App.css"
 
 export default function WeatherTemperature(props) {
-    return (
-    <span className="WeatherTemperature">
-      <span className="temperature"> {props.fahrenheit}</span>
-      <span className="unit">°F</span>
-    </span>
-    )
+
+    const [unit, setUnit] = useState("fahrenheit");
+
+        function convertToC(event) {
+
+        event.preventDefault();
+
+        setUnit("celsius");
+
+    }
+
+
+        function showF(event) {
+
+        event.preventDefault();
+
+        setUnit("fahrenheit");
+
+    }
+
+    if (unit === "fahrenheit") {
+
+        return (
+        <span className="WeatherTemperature">
+            <span className="temperature"> {Math.round(props.fahrenheit)}</span>
+            <span className="unit">°F | </span><a onClick={convertToC}>°C</a>
+        </span>
+        )
+
+    }
+
+    else {
+
+        let celsius = (props.fahrenheit - 32) * 5/9
+
+        return (
+
+            <span className="WeatherTemperature">
+                <span className="temperature"> {Math.round(celsius)}</span>
+                <span className="unit">°C | </span><a onClick={showF}>°F</a>
+            </span>
+        )
+    }
+
 }
