a5122e228541a632eb40e13d07b5b4491e1d342b
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-24 10:51:33 -0400

    Styled daily forecast
    Styled daily forecast for one day. Will add in full week using a loop.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,19 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherForecast() {
-    return <p>Forecast</p>
+    return (
+    <div className="forecast">
+
+        <div className="col">
+            <p className="forecastDay">Monday</p>
+            <WeatherIcon code="10d" />
+            <p>
+                <span className="forecastTempMin">10</span>
+                 / <span className="forecastTempMax">20</span>
+            </p>
+        </div>
+
+    </div>
+    )
 }
