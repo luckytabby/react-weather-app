2131f2e13779e8bd83b1b95745d30660275e5d8f
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-05-24 10:21:13 -0400

    Created WeatherForecast component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
-import Weather from './Weather'
+import Weather from './Weather';
+import WeatherForecast from "./WeatherForecast";
 
 function App() {
   return (
     <div id="appContainer" className="container">
         <h1>Pug Weather</h1>
-        <Weather defaultCity="Leavenworth"/>
+        <Weather defaultCity="Leavenworth" />
+        <WeatherForecast defaultCity="Leavenworth" />
         <p className="signature">
             This project is <a href="https://github.com/maggiegmcd/react-weather-app" target="_blank" rel="noreferrer">open-sourced</a> and 
             coded with ♥︎ by <a href="https://www.linkedin.com/in/maggiegmcd/" target="_blank" rel="noreferrer">Maggie McDonald</a>.
---
+++ src/WeatherForecast.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WeatherForecast() {
+    return <p>Forecast</p>
+}
